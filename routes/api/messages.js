07af2554881365a2d4802ab93b5443dea8e58fd6
MinhLong215 <148680972+MinhLong215@users.noreply.github.com>
2024-09-02 18:33:44 +0700

    update message inbox,  add loadingSpinner

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -1,34 +1,43 @@
 const express = require('express');
-const app = express();
 const router = express.Router();
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const User = require('../../schemas/UserSchema');
 const Post = require('../../schemas/PostSchema');
 const Chat = require('../../schemas/ChatSchema');
 const Message = require('../../schemas/MessageSchema');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.urlencoded({ extended: false }));
 
 router.post("/", async (req, res, next) => {
-    if(!req.body.content || !req.body.chatId) {
+    if (!req.body.content || !req.body.chatId) {
         console.log("Invalid data passed into request");
         return res.sendStatus(400);
     }
 
-    var newMessage = {
+    const newMessage = {
         sender: req.session.user._id,
         content: req.body.content,
         chat: req.body.chatId
     };
 
-    Message.create(newMessage)
-    .then(message => {
+    try {
+        // Create a new message
+        let message = await Message.create(newMessage);
+
+        // Populate 'sender' and 'chat' fields
+        message = await Message.findById(message._id)
+            .populate("sender")
+            .populate("chat")
+            .exec();
+
+        // Update the latest message in the chat
+        await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: message });
+
         res.status(201).send(message);
-    })
-    .catch(error => {
+    } catch (error) {
         console.log(error);
         res.sendStatus(400);
-    })
-})
+    }
+});
 
 module.exports = router;
--- schemas/ChatSchema.js
+++ schemas/ChatSchema.js
@@ -5,7 +5,7 @@ const chatShema = new Schema({
     chatName: { type: String, trim: true },
     isGroupChat: { type: Boolean, default: false },
     users: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-    lastestMessage: { type: Schema.Types.ObjectId, ref: 'Message' }
+    latestMessage: { type: Schema.Types.ObjectId, ref: 'Message' }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Chat', chatShema);

fb6f4ea075e1c518559372f176d89cc105be9664
MinhLong215 <148680972+MinhLong215@users.noreply.github.com>
2024-10-07 11:28:38 +0700

    Add tests or remove test step from CI/CD

--- backend/__tests__/app.test.js
+++ backend/__tests__/app.test.js
@@ -1,9 +1,14 @@
 const request = require('supertest');
-const app = require('../app'); // Đảm bảo đường dẫn đến file app.js của bạn
+const app = require('../app');
 
 describe('GET /', () => {
     it('should return 200 OK', async () => {
-        const res = await request(app).get('/');
+    // Thêm mã xác thực nếu cần thiết
+        const agent = request.agent(app);
+        await agent.post('/login') // giả định rằng bạn có một endpoint đăng nhập
+            .send({ username: 'milesmorales', password: 'user@1' }); // Điền thông tin đăng nhập
+
+        const res = await agent.get('/');
         expect(res.statusCode).toBe(200);
     });
 });
